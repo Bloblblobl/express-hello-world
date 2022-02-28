1ebc5af101bb1068a7ebf990379de22d7d4921eb
Saar Sayfan <saarsayfan@gmail.com>
2022-02-27 20:55:20 -0800

    creating the server step 2: GET handler

--- app.js
+++ app.js
@@ -9,4 +9,9 @@ const pool = new Pool({connectionString});
 
 app.use(express.json());
 
+app.get('/notes', asyncHandler(async (req, res) => {
+    const result = await pool.query('SELECT * FROM notes;');
+    res.json({ notes: result.rows });
+}));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
